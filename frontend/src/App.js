9da59e0bb25f9a3e564d55d44ca66938d644ef38
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2022-05-07 19:00:52 +0300

    react downgrade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,29 @@
-import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import { getThemeOptions } from "./resources/theme";
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { getThemeOptions } from './resources/theme';
 
 //Pages
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import ResetPassword from "./pages/auth/ResetPassword";
-import Home from "./pages/main/Home";
-import NotFound from "./pages/main/NotFound";
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import ResetPassword from './pages/auth/ResetPassword';
+import Home from './pages/main/Home';
+import Profile from './pages/main/Profile';
+import NotFound from './pages/main/NotFound';
 
 export const ColorModeContext = React.createContext({
   toggleColorMode: () => {},
 });
 
 const App = () => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const [mode, setMode] = React.useState(prefersDarkMode ? "dark" : "light");
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = React.useState(prefersDarkMode ? 'dark' : 'light');
   const colorMode = React.useMemo(
     () => ({
       // The dark mode switch would invoke this method
       toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
       },
     }),
     []
@@ -41,6 +42,7 @@ const App = () => {
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
             <Route path="reset-pass" element={<ResetPassword />} />
+            <Route path="profile" element={<Profile />} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>
