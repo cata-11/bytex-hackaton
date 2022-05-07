9dd5485af139ee7ce02c5d0359865a5ea69be6d9
cata <catalin.11.munteanu@gmail.com>
2022-05-07 13:30:01 +0300

    backend setup

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+
+// const cors = require('cors');
+
+// const bodyParser = require('body-parser');
+
+// const { body, validationResult } = require('express-validator');
+
+console.log('hello from node');
+console.log('hello from node');
