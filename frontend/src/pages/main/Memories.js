23529312d3d9a635bfe60be2cecef36bfac97aa5
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2022-05-08 03:19:15 +0300

    map and memories

--- frontend/src/pages/main/Memories.js
+++ frontend/src/pages/main/Memories.js
@@ -1,8 +1,34 @@
-import React from "react";
-import PageLayout from "./PageLayout";
+import React from 'react';
+import PageLayout from './PageLayout';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 const Memories = () => {
-  return <PageLayout></PageLayout>;
+  return (
+    <PageLayout>
+      <ImageList
+        // Reemove scrollbar
+        sx={{ height: '97%' }}
+        cols={3}
+        // rowHeight={164}
+        variant="quilted"
+      >
+        {/* Remove scroolbar */}
+        {[...Array(20)].map((x, i) => (
+          <ImageListItem key={i}>
+            <img
+              src={'https://source.unsplash.com/random/' + i}
+              // srcSet={'https://source.unsplash.com/random'}
+              // alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar position="bottom" title={'1 APR'} />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </PageLayout>
+  );
 };
 
 export default Memories;
--- frontend/src/pages/main/Profile.js
+++ frontend/src/pages/main/Profile.js
@@ -1,8 +1,22 @@
-import React from "react";
-import PageLayout from "./PageLayout";
+import React from 'react';
+import PageLayout from './PageLayout';
+import { Avatar, Typography, Box } from '@mui/material';
 
 const Profile = () => {
-  return <PageLayout></PageLayout>;
+  return (
+    <PageLayout>
+      <Box
+        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      >
+        <Avatar
+          src="https://avatars.dicebear.com/api/personas/pinu.svg"
+          style={{ width: '100px', height: '100px' }}
+        />
+        <Typography variant="h4">James Bond</Typography>
+        <Typography style={{ fontSize: '17px' }}>@jamesbond</Typography>
+      </Box>
+    </PageLayout>
+  );
 };
 
 export default Profile;
