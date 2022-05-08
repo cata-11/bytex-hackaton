be21e71044c09863a2cc3777da673f8ee77f6755
Alin Gavriliu <alin_gavriliu@yahoo.com>
2022-05-08 08:49:17 +0300

    Implemented profile level screen

--- frontend/src/resources/theme.js
+++ frontend/src/resources/theme.js
@@ -5,10 +5,12 @@ export const getThemeOptions = (mode) => ({
       ? {
           // palette values for light mode
           //   background: "#f2f2f2",
+          primary: { main: "#342fae" },
         }
       : {
           // palette values for dark mode
           //   background: "#1c2128",
+          // primary: "#342fae",
         }),
   },
 });
