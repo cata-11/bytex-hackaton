af74c11e7c5c155983f5384ce8a4903c2e4d4e55
Alin Gavriliu <alin_gavriliu@yahoo.com>
2022-05-08 03:27:37 +0300

    [WIP] Profile page

--- frontend/src/pages/main/Profile.js
+++ frontend/src/pages/main/Profile.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import PageLayout from './PageLayout';
-import { Avatar, Typography, Box } from '@mui/material';
+import React from "react";
+import PageLayout from "./PageLayout";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
 
 const Profile = () => {
   return (
     <PageLayout>
       <Box
-        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "5vh",
+        }}
       >
-        <Avatar
-          src="https://avatars.dicebear.com/api/personas/pinu.svg"
-          style={{ width: '100px', height: '100px' }}
-        />
-        <Typography variant="h4">James Bond</Typography>
-        <Typography style={{ fontSize: '17px' }}>@jamesbond</Typography>
+        <Avatar sx={{ height: "75px", width: "75px" }}>M</Avatar>
+        <Typography mt="20px" sx={{ fontSize: "20px" }}>
+          @ppinu17
+        </Typography>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget
+          leo laoreet, congue mi sed, dictum enim. Duis dictum at nisl ac
+          hendrerit. Cras at vehicula leo. Proin sed suscipit dolor, a auctor
+          nulla.
+        </Typography>
       </Box>
     </PageLayout>
   );
