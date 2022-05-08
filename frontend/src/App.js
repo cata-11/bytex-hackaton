92fd94b687333f6db1b8fea5946586afafc8d328
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2022-05-08 11:07:07 +0300

    redeem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,28 +5,16 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import { getThemeOptions } from './resources/theme';
 
 //Pages
-<<<<<<< HEAD
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import ResetPassword from './pages/auth/ResetPassword';
-import Feed from './pages/main/Feed';
-import Memories from './pages/main/Memories';
-import LeaderBoard from './pages/main/LeaderBoard';
-import Profile from './pages/main/Profile';
+import PageLayout from './pages/main/PageLayout';
 import NotFound from './pages/main/NotFound';
 import Notifications from './pages/main/Notifications';
-=======
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import ResetPassword from "./pages/auth/ResetPassword";
-import PageLayout from "./pages/main/PageLayout";
-import NotFound from "./pages/main/NotFound";
-import Notifications from "./pages/main/Notifications";
-import ProtectedRoute from "./ProtectedRoute";
->>>>>>> b41a26f9120e6ac5336828e526e8a0fe5c9ca318
+import ProtectedRoute from './ProtectedRoute';
 
 export const ColorModeContext = React.createContext({
-  toggleColorMode: () => {}
+  toggleColorMode: () => {},
 });
 
 const App = () => {
@@ -37,7 +25,7 @@ const App = () => {
       // The dark mode switch would invoke this method
       toggleColorMode: () => {
         setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      }
+      },
     }),
     []
   );
@@ -50,20 +38,12 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <Routes>
-<<<<<<< HEAD
-            <Route path="feed" element={<Feed />} />
-            <Route path="scores" element={<LeaderBoard />} />
-            <Route path="memories" element={<Memories />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="notifications" element={<Notifications />} />
-=======
             <Route exact path="/" element={<ProtectedRoute />}>
               <Route path="/home" element={<PageLayout />} />
               <Route path="notifications" element={<Notifications />} />
               <Route path="*" element={<NotFound />} />
             </Route>
 
->>>>>>> b41a26f9120e6ac5336828e526e8a0fe5c9ca318
             <Route path="login" element={<Login />} />
             <Route path="signup" element={<Register />} />
             <Route path="reset-pass" element={<ResetPassword />} />
--- frontend/src/pages/main/Profile.js
+++ frontend/src/pages/main/Profile.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
-import Divider from "@mui/material/Divider";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
+import Events from '../../components/Events';
 
-import UserContext from "../../resources/context/UserContext";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
+import UserContext from '../../resources/context/UserContext';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
-import UserLevel from "./UserLevel";
-import Friends from "../../components/Friends";
-import { useContext } from "react";
+import UserLevel from './UserLevel';
+import Friends from '../../components/Friends';
+import { useContext } from 'react';
+const axios = require('axios');
 
 const Profile = () => {
   const [value, setValue] = React.useState(0);
+  const [score, setScore] = React.useState(0);
+
   const userCtx = useContext(UserContext);
 
   const handleChange = (event, newValue) => {
@@ -22,42 +26,54 @@ const Profile = () => {
   };
 
   const username = userCtx.getUsername();
-  const score = userCtx.getScore();
   const fullName = userCtx.getFullName();
+
+  useEffect(() => {
+    (async () => {
+      await axios
+        .get('http://localhost:5000/users/' + localStorage.getItem('username'))
+        .then((res) => {
+          console.log(res.data.user.score);
+          setScore(res.data.user.score);
+        });
+    })();
+  }, []);
 
   return (
     <>
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          marginTop: "5vh",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: '5vh',
         }}
       >
         <Avatar
-          sx={{ height: "75px", width: "75px" }}
-          src={"https://avatars.dicebear.com/api/personas/" + username + ".svg"}
+          sx={{ height: '75px', width: '75px' }}
+          src={'https://avatars.dicebear.com/api/personas/' + username + '.svg'}
         />
-        <Typography mt="20px" sx={{ fontSize: "25px" }}>
+        <Typography mt="20px" sx={{ fontSize: '25px' }}>
           {fullName}
         </Typography>
-        <Typography mt="2px" sx={{ fontSize: "15px" }}>
-          {"@" + username}
+        <Typography mt="2px" sx={{ fontSize: '15px' }}>
+          {'@' + username}
         </Typography>
-        <Typography mt="10px" sx={{ fontSize: "20px" }}>
-          {"Score: " + score}
+        <Typography mt="10px" sx={{ fontSize: '20px' }}>
+          {'Score: ' + score}
         </Typography>
       </Box>
-      <Divider sx={{ marginTop: "20px" }} />
-      <Box sx={{ width: "100%" }}>
+      <Divider sx={{ marginTop: '20px' }} />
+      <Box sx={{ width: '100%' }}>
         <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Level" sx={{ textTransform: "none" }} />
-          <Tab label="Friends" sx={{ textTransform: "none" }} />
+          <Tab label="Level" sx={{ textTransform: 'none' }} />
+          <Tab label="Friends" sx={{ textTransform: 'none' }} />
+          <Tab label="My Events" sx={{ textTransform: 'none' }} />
         </Tabs>
       </Box>
       {value === 0 && <UserLevel />}
       {value === 1 && <Friends />}
+      {value === 2 && <Events />}
     </>
   );
 };
